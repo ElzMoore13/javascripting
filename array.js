37feb354bbd08cdb4fe2b8c2bd6afbd29e7c6eb2
Eliza Moore <elzmoore13@gmail.com>
2018-05-10 12:57:36 +0000

    Initial commit with all work so far

---
+++ array.js
@@ -0,0 +1,6 @@
+var pizzaToppings = ['tomato sauce', 'cheese', 'pepperoni'];
+console.log(pizzaToppings);
+console.log('\n');
+for(var i = 0; i < pizzaToppings.length; i++){
+  console.log(pizzaToppings[i]);
+}
